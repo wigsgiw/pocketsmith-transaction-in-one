014d135d9756e0795e12bc58c66ccbf371a203be
James Wigglesworth <james+eos@pocketsmith.com>
2016-06-03 01:33:54 +1200

    Quickie /me page - using secrets.yml in dev - relying on ENV variables being set in prod (Heroku) - using httparty for our httpartying

--- app.rb
+++ app.rb
@@ -1,5 +1,23 @@
 require 'sinatra'
+require 'httparty'
+
+before do
+  # If we're in development we need to load things into env
+  # If we're in production in Heroku, no need, as they'll be set as part of setup
+  if Sinatra::Base.development?
+    secrets = YAML.load_file('secrets.yml')
+    secrets.each do |key, value|
+      ENV[key] = value
+    end  
+  end
+  
+end
 
 get '/' do
   "Oh, the wonderful things we'll do together."
+end
+
+get '/me' do
+  response = HTTParty.get('https://api.pocketsmith.com/v2/me', headers: {"Authorization" => "Key #{ENV['pocketsmith_developer_key']}"})
+  response.body
 end
