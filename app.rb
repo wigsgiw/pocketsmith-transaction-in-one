e7d16271c535248f1a495d061f872a695d6441e6
James Wigglesworth <james+eos@pocketsmith.com>
2016-06-03 03:16:44 +1200

    First cut of creating transactions, using skeleton.css for style

--- app.rb
+++ app.rb
@@ -7,14 +7,29 @@ before do
   if Sinatra::Base.development?
     secrets = YAML.load_file('secrets.yml')
     secrets.each do |key, value|
-      ENV[key] = value
-    end  
+      ENV[key] = value.to_s
+    end
   end
   
 end
 
 get '/' do
-  "Oh, the wonderful things we'll do together."
+  erb :index, layout: :layout
+end
+
+post '/create' do
+  amount = params[:amount] == "" ? nil : params[:amount].to_f.abs * -1
+  payee  = params[:payee] == "" ? ENV['pocketsmith_default_merchant'] : params[:payee]
+
+  transaction_params = {
+    :date => Time.now,
+    :amount => amount,
+    :payee => payee
+  }
+  
+  @resp = HTTParty.post("https://api.pocketsmith.com/v2/transaction_accounts/#{ENV['pocketsmith_transaction_account_id']}/transactions", body: transaction_params, headers: {"Authorization" => "Key #{ENV['pocketsmith_developer_key']}"})
+
+  erb :create, layout: :layout
 end
 
 get '/me' do
